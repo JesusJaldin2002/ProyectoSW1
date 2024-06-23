c6216952b794b7946af12e79c51fe34c49bb61d1
Jesus Jaldin <jesusjaldin2002@gmail.com>
2024-06-23 17:07:22 -0400

    update port

--- resources/views/chats/show.blade.php
+++ resources/views/chats/show.blade.php
@@ -95,7 +95,7 @@
 @section('scripts')
     <script type="module" src="{{ asset('js/script3.js') }}"></script>
     <script>
-        const socket = io('http://54.83.154.221:80');
+        const socket = io('http://54.83.154.221:8080');
 
         document.getElementById('chat-form').addEventListener('submit', function(e) {
             e.preventDefault();
