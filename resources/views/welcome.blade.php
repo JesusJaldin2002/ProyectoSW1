50720f6149874711446042900d342c08426877a1
Jesus Jaldin <jesusjaldin2002@gmail.com>
2024-06-22 22:01:20 -0400

    "Updated links to styles.css and script.js to use relative paths instead of asset helper"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,16 +12,17 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
 
     <!-- Todo esto es necesario para la detección de las señas -->
     <script src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@mediapipe/control_utils/control_utils.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@mediapipe/holistic/holistic.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm/dist/tf-backend-wasm.js"></script>
     <!-- Todo esto es necesario para la detección de las señas -->
+
 </head>
 
 <body class="antialiased">
@@ -36,7 +37,7 @@
     </div>
 
     <!-- Este es el script.js principal para lenguaje de señas -->
-    <script type="module" src="{{ asset('js/script.js') }}"></script>
+    <script type="module" src="js/script.js"></script>
 </body>
 
 </html>
