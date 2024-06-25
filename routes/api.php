89844e489bb9b93e80a003d67ad1e6bf0ed5801d
Jesus Jaldin <jesusjaldin2002@gmail.com>
2024-06-24 22:29:27 -0400

    "Added LearningController and new API route for /learning"

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\LearningController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     //Users
     Route::post('/logout', [AuthController::class, 'logout']);
     
-
+    Route::get('/learning', [LearningController::class,'get']);
 
 });
